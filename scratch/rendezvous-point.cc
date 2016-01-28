a7c5524f0c303b83c2405861df06eded4c77d4e0
joakim <joakim.karl.larsson@gmail.com>
2016-01-28 13:30:28 +0100

    start of rendezvouspoint logging in plus

---
+++ scratch/rendezvous-point.cc
@@ -0,0 +1,37 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "ns3/core-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/netanim-module.h"
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE ("rendezvous-point");
+
+class RendezvousPoint
+{
+  //construct
+  public:
+    RendezvousPoint(Vector pos);
+    Vector pos = pos;
+    Vector connection; //between
+    Vector nodes;
+
+  void setPath(Vector path, RendezvousPoint rp) {
+
+  }
+};
+
